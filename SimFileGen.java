15bab0e56e1a67f39a5015408255ac3650cce847
goo3r <mtgyure@gmail.com>
2014-03-12 22:29:19 -0600

    Finished File maker
    Soooo done

--- SimFileGen.java
+++ SimFileGen.java
@@ -1,36 +1,50 @@
+//Matthew Gyure
+//Jaime Alvarez
+//Emmanuel Valdivia
+//ECE 448 - Operating System
+//Project 1
+//
+//Program Name: SimFileGen
+//Language: Java
+//Last Updated: 3/12/2014
+//Description: This program generates a text file of 30 random prcoesses. The arrival times and process times are all random as to simulate a set of processes an operating system would encounter.
+
 import java.util.*;
 import java.io.*;
 
 public class SimFileGen{
 	
-	public SimFileGen(){
-		String processName;
-		int arrivalTime;
-		int processTime;
-		int processCount;
-	}
 
 	public static void main(String args[]){
 		SimFileGen test = new SimFileGen();
-		test.writeFile(true);
+		int processCount = 0;
+
+		while(processCount<30){
+			if(processCount == 0){
+				test.writeFile(false, processCount);
+			}else{
+				test.writeFile(true, processCount);
+			}
+			processCount++;
+		}
 	}
 	
-	public void writeFile(boolean closer) {
-    		String fileName = "test.txt";
+	public void writeFile(boolean append, int processCount) {
+    		String fileName = "RandomProcesses.txt";
      
     		try {
-    			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName,false));
-    			writer.write(processName+","+arrivalTime+","+processTime);
+    			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName,append));
+    			writer.write("Process#"+processCount+","+randoGen()+","+randoGen());
     			writer.newLine();
-    			if(closer){
-				writer.close();
-			}
+			writer.close();
     		    } catch (IOException e) {
     			e.printStackTrace();
     		    }
     	}
 
-	public void randoGen(){
+	public int randoGen(){
 		Random rand = new Random();
+		
+		return rand.nextInt(10);
 	}
 }
